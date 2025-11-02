67a994c8d026523226bc951077f12c6acad86613
Yusuke <ysk2645j4@gmail.com>
2025-11-02 15:19:37 +0900

    ルーティング設定追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter,createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import MenuPage from '../pages/MenuPage.vue';
 
@@ -6,18 +6,48 @@ const routes = [
   {
     path: '/',
     component: HomePage,
-    name:'Home'
+    name: 'Home',
   },
   {
-    path:'/menu',
+    path: '/about',
+    component: AboutPage,
+    name: 'About',
+  },
+  {
+    path: '/faq',
+    component: FaqPage,
+    name: 'Faq',
+  },
+  {
+    path: '/access',
+    component: AccessPage,
+    name: 'Access',
+  },
+  {
+    path: '/menu',
     component: MenuPage,
-    name:'Menu'
-  }
+    name: 'Menu',
+  },
+  {
+    path: '/menu-detail',
+    component: MenuDetailPage,
+    name: 'MenuDetail',
+  },
+  {
+    path: '/contact',
+    component: ContactPage,
+    name: 'Contact',
+  },
+  {
+    path: '/privacy',
+    component: PrivacyPage,
+    name: 'Privacy',
+  },
 ];
 
 const router = createRouter({
-  history:createWebHistory(),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
