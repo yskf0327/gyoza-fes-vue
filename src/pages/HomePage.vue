741b8484e66e1738ea819c43e435a94b8fc59b6d
Yusuke <ysk2645j4@gmail.com>
2025-11-02 14:32:24 +0900

    トップページ、メニューページ追加

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hero">
+    <h1>トップページ</h1>
+  </div>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+  body{
+    background-color: #fff;
+  }
+</style>
---
+++ src/pages/MenuPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MenuList from '../components/MenuList.vue'
+</script>
+
+<template>
+  <div class="hero">
+    <h1>メニューページ</h1>
+  </div>
+  <div class="container">
+    <MenuList></MenuList>
+  </div>
+</template>
